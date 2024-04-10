df7cd4df534c2f59d045347f6d07d88e363c38c7
akullu winnifred <akulluwinnifred1@gmail.com>
2024-04-10 09:01:00 +0300

    created contact and resources page

--- refactory/refactory/urls.py
+++ refactory/refactory/urls.py
@@ -22,4 +22,6 @@ urlpatterns = [
     # path('admin/', admin.site.urls),
     path('', include('home.urls')),
     path('', include('about.urls')),
+    path('',include('contact.urls')),
+     path('',include('resources.urls')),
 ]
